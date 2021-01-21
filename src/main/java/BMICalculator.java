d1c28669a6d121be26fd664104eeac12e67952ab
alla_trinda <alla@mailinator.com>
2021-01-21 21:33:29 +0300

    Initial commit

---
+++ src/main/java/BMICalculator.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BMICalculator {
+
+    @Test
+    public void calcTEst() {
+        open("https://healthunify.com/bmicalculator/");
+        $(By.name("wg")).sendKeys("85");
+        $(By.name("ht")).sendKeys("185");
+        $(By.name("cc")).click();
+        String result = $(By.name("desk")).getAttribute("value");// изучить getAttribute
+        Assert.assertEquals(result, "Your category is Normal");
+    }
+}
